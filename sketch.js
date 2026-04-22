885bd3c28ff798064ad73ad619ddb5fd3624765b
tuberolink <151033406+tuberolink@users.noreply.github.com>
2026-04-22 11:09:47 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let audioPlayer;
+
+function setup() {
+  noCanvas();
+
+  // sfondo nero pagina
+  document.body.style.background = "black";
+  document.body.style.margin = "0";
+  document.body.style.height = "100vh";
+
+  audioPlayer = createAudio('/assets/tono2.mp3');
+
+  audioPlayer.attribute(
+    'aria-description',
+    'Audio player standard.'
+  );
+
+  audioPlayer.showControls();
+
+  // 🎯 centratura (senza scale)
+  audioPlayer.style('position', 'absolute');
+  audioPlayer.style('top', '50%');
+  audioPlayer.style('left', '50%');
+  audioPlayer.style('transform', 'translate(-50%, -50%)');
+}
+
+function draw() {
+  // nessuna modifica
+}
